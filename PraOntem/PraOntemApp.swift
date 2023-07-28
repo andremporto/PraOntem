374f7d0d7599e06d17e2855d06e310c1997f2488
André Porto <andreporto@me.com>
2023-07-28 15:24:18 -0300

    Initial Commit

---
+++ PraOntem/PraOntemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraOntemApp.swift
+//  PraOntem
+//
+//  Created by André Porto on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PraOntemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
